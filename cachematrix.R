b16d2264fce943f5c7b7fbc4889eb595f14bf0ef
palkaramol <palkaramol@gmail.com>
2020-03-05 18:49:38 +0530

    Computing the inverse of a square matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -17,7 +14,9 @@ makeCacheMatrix <- function(x = matrix()) {
        setinverse = setinverse,
        getinverse = getinverse)
 }
-## Write a short comment describing this function
+
+
+
 
 cacheSolve <- function(x, ...) {
   i <- x$getinverse()
@@ -30,7 +29,7 @@ cacheSolve <- function(x, ...) {
   x$setinverse(i)
   i
 }
-        ## Return a matrix that is the inverse of 'x'
+      
 
 B <- matrix(c(1,2,3,4),2,2)
 
